77c7cd8f8b05952af77ff72f24cdf548fb6f53cf
dsmith <a@a>
2016-11-16 20:18:12 -0800

    I commit this project

---
+++ routes/account_routes.js
@@ -0,0 +1,39 @@
+/**
+ * Created by student on 11/16/16.
+ */
+var express = require('express');
+var router = express.Router();
+var account_dal = require('../model/account_dal');
+
+
+// View All acounts
+router.get('/all', function(req, res) {
+    account_dal.getAll(function(err, result){
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.render('account/accountViewAll', { 'result':result });
+        }
+    });
+
+});
+
+// View the school for the given id
+router.get('/', function(req, res){
+    if(req.query.account_id == null) {
+        res.send('account_id is null');
+    }
+    else {
+        school_dal.getById(req.query.school_id, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.render('account/accountViewById', {'result': result});
+            }
+        });
+    }
+});
+
+module.exports = router;
