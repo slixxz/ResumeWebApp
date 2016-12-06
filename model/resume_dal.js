a81863224e13a6d3ba9ce302883dd053f9148d73
Dave <q@1>
2016-12-06 15:46:53 -0800

    lab 11 work in prgress

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -5,7 +5,7 @@ var db  = require('./db_connection.js');
 var connection = mysql.createConnection(db.config);
 
 exports.getAll = function(callback) {
-    var query = 'SELECT * FROM resume;';
+    var query = 'SELECT * FROM resume_view;';
 
     connection.query(query, function(err, result) {
         callback(err, result);
@@ -13,10 +13,35 @@ exports.getAll = function(callback) {
 };
 
 exports.getById = function(resume_id, callback) {
-    var query = 'SELECT * FROM resume WHERE resume_id = ?';
+    var query = 'SELECT * FROM resume_view WHERE resume_id = ?';
     var queryData = [resume_id];
 
     connection.query(query, queryData, function(err, result) {
         callback(err, result);
     });
+};
+
+exports.insert = function(params, callback) {
+    var query = 'INSERT INTO resume (resume_id, account_id) VALUES (?, ?,?)';
+
+    // the question marks in the sql query above will be replaced by the values of the
+    // the data in queryData
+    var queryData = [params.resume_id, params.account_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+}
+
+exports.delete = function(resume_id, callback) {
+    console.log(resume_id);
+
+    var query = 'DELETE FROM resume WHERE resume_id = ?';
+    var queryData = [resume_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
 };
--- routes/resume_routes.js
+++ routes/resume_routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var resume_dal = require('../model/resume_dal');
+var account_dal = require('../model/account_dal');
 
 
 // View All schools
@@ -32,5 +33,64 @@ router.get('/', function(req, res){
         });
     }
 });
+
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    resume_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('resume/resumeAdd', {'resume': result});
+        }
+    });
+});
+
+// insert a school record
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.resume_name == null) {
+        res.send('School Name must be provided.');
+    }
+    else if(req.query.resume_id == null) {
+        res.send('An Address must be selected');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        resume_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/resume/all');
+            }
+        });
+    }
+});
+
+
+
+// Delete a school for the given school_id
+router.get('/delete', function(req, res){
+    console.log("data: ", req.query);
+
+    if(req.query.resume_id == null) {
+        res.send('resume_id is null');
+    }
+    else {
+        resume_dal.delete(req.query.resume_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/resume/all');
+            }
+        });
+    }
+});
+
+
 
 module.exports = router;
